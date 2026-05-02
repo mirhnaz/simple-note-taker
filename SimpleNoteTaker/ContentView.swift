a3c9d55524c7d56bfc228ca7aaf41394ee54b728
Naz Mir <mirnaz.hussain@gmail.com>
2026-05-02 12:53:59 +0530

    Initial Commit

---
+++ SimpleNoteTaker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleNoteTaker
+//
+//  Created by Naz Mir on 02/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleNoteTaker/SimpleNoteTakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleNoteTakerApp.swift
+//  SimpleNoteTaker
+//
+//  Created by Naz Mir on 02/05/26.
+//
+
+import SwiftUI
+
+@main
+struct SimpleNoteTakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
